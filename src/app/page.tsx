7629ea2dfa6541aee89d0457fdb9cecef79b02cf
Andy W <37781802+aandyw@users.noreply.github.com>
2025-03-29 13:32:16 -0400

    setup shadcn ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,13 @@
 import React, { useEffect } from "react";
 import ChatbotSDK from "../sdk";
 
+import {
+  Card,
+  CardHeader,
+  CardContent,
+  CardFooter,
+} from "@/components/ui/card";
+
 const DemoPage: React.FC = () => {
   useEffect(() => {
     // Initialize the chatbot
@@ -24,12 +31,12 @@ const DemoPage: React.FC = () => {
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24">
-        <body>
-          <div style={{ maxWidth: "800px", margin: "0 2rem", padding: "2rem" }}>
-            <h1>Your Biggest Fan</h1>
-            <p>•ᴗ•</p>
-          </div>
-        </body>
+      <body>
+        <div style={{ maxWidth: "800px", margin: "0 2rem", padding: "2rem" }}>
+          <h1>Your Biggest Fan</h1>
+          <p>•ᴗ•</p>
+        </div>
+      </body>
     </main>
   );
 };
